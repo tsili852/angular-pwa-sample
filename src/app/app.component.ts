9bfa4ce8e5d9e5ac01fc42296468eb7cf07c28ad
Nick Tsilivis <nick.tsilivis@gmail.com>
2018-11-14 15:59:40 +0100

    Initial Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-pwa-sample';
+  promptEvent: any;
+
+  title = 'Angular PWA Sample';
+
+  constructor(
+    private readonly swUpdate: SwUpdate,
+    private readonly snackBar: MatSnackBar
+  ) {
+    let noInternetSnack;
+
+    window.addEventListener('online', () => {
+      noInternetSnack.dismiss();
+    });
+
+    window.addEventListener('offline', () => {
+      noInternetSnack = this.snackBar.open('No Internet connection', 'Ok');
+    });
+
+    if (this.swUpdate.isEnabled) {
+      this.snackBar.open('Service Workers enabled', 'Ok', { duration: 3000 });
+    }
+
+    window.addEventListener('beforeinstallprompt', event => {
+      event.preventDefault();
+
+      this.promptEvent = event;
+      const snackInstall = this.snackBar.open('Do you want to install the application to your device ?', 'Install', { duration: 3000 });
+
+      snackInstall.onAction().subscribe(() => {
+        this.promptEvent.prompt();
+        this.promptEvent.userChoice.then(choiceResult => {
+          if (choiceResult.outcome === 'accepted') {
+            this.snackBar.open('PWA install accepted', 'Ok', { duration: 3000 });
+          } else {
+            this.snackBar.open('PWA install dismissed', 'Ok', { duration: 3000 });
+          }
+        });
+      });
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatSnackBarModule } from '@angular/material';
+import { MatToolbarModule } from '@angular/material';
+import { MatCardModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSnackBarModule,
+    MatToolbarModule,
+    MatCardModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
